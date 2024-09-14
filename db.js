d419aa546c5d50a3343c9bd039344ac4e43c2d07
Qilton <swayambhalotia@gmail.com>
2024-09-14 22:30:53 +0530

    yt

--- db.js
+++ db.js
@@ -1,12 +1,14 @@
-
+const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-const mongo_url ="mongodb+srv://Swayam:9832900366@cluster0.z7kyt.mongodb.net/multer"
-;
 
+dotenv.config();
+
+const mongo_url = process.env.MONGO_URI;
 
 mongoose.connect(mongo_url)
     .then(() => {
         console.log('MongoDB Connected...');
-    }).catch((err) => {
-        console.log('MongoDB Connection Error: ', err);
     })
+    .catch((err) => {
+        console.log('MongoDB Connection Error: ', err);
+    });
