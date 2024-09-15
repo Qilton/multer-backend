6bf50c5c85e8f3b87f9b16ce260f21216117e7d9
Qilton <swayambhalotia@gmail.com>
2024-09-15 12:55:27 +0530

    gds

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 dotenv.config();
 
-const mongo_url =process.env.MONGO_CONN
+const mongo_url ="mongodb+srv://Swayam:9832900366@cluster0.z7kyt.mongodb.net/multer"
 
 mongoose.connect(mongo_url)
     .then(() => {
